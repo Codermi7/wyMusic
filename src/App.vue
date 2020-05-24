58ebcd9038b3ad12617f1fc63bb574af8c3585dd
Your Name <you@example.com>
2020-05-24 13:22:21 +0700

    '播放'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <transition name="fade">
+      <component :is="showCom"></component>
+    </transition>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import Play from './components/play'
+import Loading from './components/loading'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      showCom:Loading
+    }
+  },
+  created() {
+    window.addEventListener('load',()=>{
+      setTimeout(() => {
+        this.showCom = Play;
+      }, 500);
+    })
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang='less'>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
 }
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+button{
+  position: fixed;
+  z-index: 19;
+}
+
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import './less/icon.less'
 Vue.config.productionTip = false
+
+
 
 new Vue({
   render: h => h(App),
